4b9eb97195551e73f151a3f0d73f348c7eb60d9c
Lakshmi Deepak <r170869@rguktrkv.ac.in>
2021-08-25 11:21:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+# Specify which file (without .py extension) in the arts folder should be used
+artFile = "default"
+# Speed of art
+speed = 0.0015
+# Print code in the beginning
+codePrint = False
+codingSpeed = 0.001
+codeColor='red'
+# Audio
+playAudio = True
+audio = 'HappyBirthday.mp3'
+# Random color is choosen from the list
+color = ['red','green','yellow','blue','magenta','cyan','white']
+# Change the keys of the dict to change the color codes
+# If you change the color codes for blink, remove blink(none) and random, you have to change it in pprint() function of PyBirthdayWish.py too.
+colorCodes = {''
+              '':'grey','②':'red','③':'green','④':'yellow','⑤':'blue','⑥':'magenta','⑦':'cyan','⑧':'white','⑨':'blink','⑩':'none','®':'random'}
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+              
+'''Credits'''     #Lakshmi Deepak
